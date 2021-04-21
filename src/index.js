904af4d1c79f28b35245c9ef0acb7ead4c733fb6
VeroFlores <veritocalla@gmail.com>
2021-04-20 23:57:37 -0500

    update category function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-const express = require('express')
+import express from 'express';
+import cors  from "cors";
+import dotenv from "dotenv";
+import mysql from "mysql";
+
 const app = express()
-const router = express.Router();
-var cors = require('cors')
-// const database = require('./database');
-const dotenv=require('dotenv');
-var mysql      = require('mysql');
 dotenv.config()
 app.use(cors());
 // setting
@@ -18,7 +17,6 @@ app.get('/', (req, res) => {
 // funciones que tienen acceso al objeto de solicitud (req), al objeto de respuesta (res) y a la siguiente función de middleware en el ciclo de solicitud/respuestas de la aplicación
 // si recibimos un json el modulo de express será capaz de entenderlo
 app.use(express.json());
-//Routes
 
 // La aplicación inicia un servidor y escucha las conexiones en el puerto 3000.
 // arriba se define y acá se utiliza 
@@ -26,7 +24,7 @@ app.use(express.json());
 app.listen(app.get('port'), () => {
   console.log('server on port',app.get('port'))
 })
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host     : process.env.DB_HOST,
   database :process.env.DB_DATABASE,
   user     : process.env.DB_USER,
@@ -36,51 +34,25 @@ connection.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
   });
-app.get('/allProducts', (request, response) => {
-  connection.query("SELECT * FROM product",(err,result)=>{
-    if (err) reject(err);
-    (response.json(result));
-  })
-})
-app.get('/category', (request, response) => {
-  connection.query("SELECT * FROM product WHERE category LIKE '%1%'",(err,result)=>{
-    if (err) throw err;
-    (response.json(result));
-  })
-})
-app.get(`/product`, (request, response) => {
-  const search = request.query.search;
-  console.log(search);
-  connection.query(`SELECT * FROM product WHERE name LIKE '%${search}%'`,(err,result)=>{
-    if (err) throw err;
-    (response.json(result));
-  })
-})
-  // connection.query('SELECT name FROM product', (error, result) => {
-  //     if (error) throw error;
 
-  //     response.send(result);
-
-  // });
-// app.get('/json', function (req, res) {
-//   connection.connect();  
- 
-//   connection.query('SELECT name FROM product', function(err, rows, fields)   
-//   {  
-//       connection.end();
- 
-//       if (err) throw err;  
- 
-//       res.json(rows); 
- 
-//   });
-// });
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error('error connecting: ' + err.stack);
-//     return;
-//   }
- 
-//   console.log('connected as id ' + connection.threadId);
-// });
+  app.get('/allProducts', (request, response) => {
+    connection.query("SELECT * FROM product",(err,result)=>{
+      if (err) reject(err);
+      (response.json(result));
+    })
+  })
+  app.get('/category', (request, response) => {
+    connection.query("SELECT * FROM category'",(err,result)=>{
+      if (err) throw err;
+      (response.json(result));
+    })
+  })
+  app.get(`/product`, (request, response) => {
+    const search = request.query.search;
+    console.log(search);
+    connection.query(`SELECT * FROM product WHERE name LIKE '%${search}%'`,(err,result)=>{
+      if (err) throw err;
+      (response.json(result));
+    })
+  })
 
---
+++ src/routes.js
@@ -0,0 +1,6 @@
+// import express from 'express';
+
+// import connection from './index.js'
+// const app = express()
+// const router = express.Router();
+
